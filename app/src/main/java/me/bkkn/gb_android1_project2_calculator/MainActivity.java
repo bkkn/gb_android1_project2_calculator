141e726794da3e0f351abba021d5c92941042d5b
bkkn_h <bakaikin@yandex.ru>
2021-12-15 21:48:39 +0300

    Initial commit

---
+++ app/src/main/java/me/bkkn/gb_android1_project2_calculator/MainActivity.java
@@ -0,0 +1,14 @@
+package me.bkkn.gb_android1_project2_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
