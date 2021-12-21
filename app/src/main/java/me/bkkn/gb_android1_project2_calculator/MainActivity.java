88d312f7fffa381838bb9498c2076ac7b11e2a8b
bkkn_m <bakaikin@yandex.ru>
2021-12-22 00:17:03 +0300

    EditText -> TextView

--- app/src/main/java/me/bkkn/gb_android1_project2_calculator/MainActivity.java
+++ app/src/main/java/me/bkkn/gb_android1_project2_calculator/MainActivity.java
@@ -3,9 +3,10 @@ package me.bkkn.gb_android1_project2_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
