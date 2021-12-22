eb7694e4d7ce30fa3b3e54ad0104b7d52f20b808
bakaikin <bakaikin@yandex.ru>
2021-12-22 16:42:19 +0300

    Tasks 4,5 completed: expression doesn't depend on rotation and is transmitted to second activity Basic tasks 1-5 seem to be completed

--- app/src/main/java/me/bkkn/gb_android1_project2_calculator/LookupActivity.java
+++ app/src/main/java/me/bkkn/gb_android1_project2_calculator/LookupActivity.java
@@ -1,14 +1,10 @@
 package me.bkkn.gb_android1_project2_calculator;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import org.w3c.dom.Text;
 
 import me.bkkn.gb_android1_project2_calculator.model.Expression;
 
