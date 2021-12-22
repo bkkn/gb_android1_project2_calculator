ca1e1ceb2be83d3fb49a5b00e1799b36f93896e6
bakaikin <bakaikin@yandex.ru>
2021-12-22 11:44:08 +0300

    trying to save state: workaround2: manifest: android:configChanges="orientation|keyboardHidden|screenSize" forbid Activity recreation

--- app/src/main/java/me/bkkn/gb_android1_project2_calculator/MainActivity.java
+++ app/src/main/java/me/bkkn/gb_android1_project2_calculator/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,6 +15,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "@@@";
     private List<Button> digitButtonList;
     private ArrayList<Integer> digitButtonResourceList;
     private TextView output;
@@ -35,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        Log.d(TAG, "onConfigurationChanged() called with: newConfig = [" + newConfig + "]");
         super.onConfigurationChanged(newConfig);
     }
 
