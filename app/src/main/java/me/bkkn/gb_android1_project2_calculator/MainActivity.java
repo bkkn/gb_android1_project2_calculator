da7a43e5d078c0f04d63ac508685728a61b7a9ef
bakaikin <bakaikin@yandex.ru>
2021-12-23 09:53:11 +0300

    inconsistent naming fixed

--- app/src/main/java/me/bkkn/gb_android1_project2_calculator/MainActivity.java
+++ app/src/main/java/me/bkkn/gb_android1_project2_calculator/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -35,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate() called with: savedInstanceState = [" + savedInstanceState + "]");
         initDigitButtons();
-        output = findViewById(R.id.screen_edit_text);
+        output = findViewById(R.id.output_text_view);
         initOperations();
         initSpecialButtons();
 
