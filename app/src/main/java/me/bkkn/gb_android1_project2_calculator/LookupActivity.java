a164802d4dfc20a0cccb115fb419cae7c75d14a9
bkkn <bakaikin@yandex.ru>
2022-01-05 23:05:49 +0300

    plus issue fixed

--- app/src/main/java/me/bkkn/gb_android1_project2_calculator/LookupActivity.java
+++ app/src/main/java/me/bkkn/gb_android1_project2_calculator/LookupActivity.java
@@ -1,6 +1,5 @@
 package me.bkkn.gb_android1_project2_calculator;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.WebView;
 
@@ -16,16 +15,11 @@ public class LookupActivity extends AppCompatActivity {
         setContentView(R.layout.lookup_activity);
 
         final WebView webView = (WebView) findViewById(R.id.web_view);
-        Intent intent = getIntent();
         Expression expression = ((Expression) getIntent().getParcelableExtra(Expression.KEY));
-        String input = expression.toString();
-        double result = expression.evaluate();
-
         String query = expression.toString();
         if (!query.contains("="))
             query += "=";
-        query.replace("+", "%2B");
-        String url = "https://www.google.com/search?q=" + query;
+        String url = "https://www.google.com/search?q=" + query.replace("+", "%2B");
         webView.loadUrl(url);
     }
 }
